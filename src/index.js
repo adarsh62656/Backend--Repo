aced8725e7da3ffa317500e26ece42a574d4778a
Adarsh62656 <adarsh62656@gmail.com>
2025-08-24 09:32:10 +0530

    MongoDB Authentication

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 
 
 console.log("PORT", process.env.PORT);
-connectDB();
+connectDB()
+.then(())=> {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is Listening on port ${process.env.PORT || 8000}`);
+    } )
+})
+.catch((error) => {
+    console.log("error in DB connection",error)
+} )
 
 
 

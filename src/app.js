97a8f3b7b015ee43182f776da34a78e821bfd01c
Adarsh62656 <adarsh62656@gmail.com>
2025-08-31 21:32:34 +0530

    First Example Controller

--- src/app.js
+++ src/app.js
@@ -12,4 +12,11 @@ app.use(express.json({ limit: "16kb" }))                       // to parse json 
 app.use(express.urlencoded({ extended: true, limit: "16kb" })) // to parse form data from request body
 app.use(express.static("public"))                              // to serve static files like images, css files, js files
 app.use(cookieParser())                                        // to parse cookies from request headers
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
 export { app }
---
+++ src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { app } from "../app.js";
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js"
+
+
+const router = Router()
+
+router.route("/register").post(registerUser)
+
+export default router

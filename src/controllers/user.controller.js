26a1a235cb87eec0c82dc34a962d9ccc25c98546
Adarsh62656 <adarsh62656@gmail.com>
2025-09-02 13:36:36 +0530

    adding LogicBuilding COde till 29:30

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,23 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 const registerUser = asyncHandler ( async (req,res) => {
-    res.status(500).json({
-        message: "OK"
+    // get the data from request body
+    const {fullname, email,username,password} = req.body
+    //validate the data from req.body weather it is Empty
+    if (
+        [fullname, email,username,password].send((field) => field.trim()==="")
+    ) {
+        throw new ApiError(409,"Field Not Found")
+    }
+    //Check if User exists
+    const existingUser = User.findOne({
+        $or: [{username},{email}]
     })
+    if (existingUser) {
+        throw new ApiError(409,"User already exists with this email or username")
+    }
+    
 })
 
 export {registerUser}
